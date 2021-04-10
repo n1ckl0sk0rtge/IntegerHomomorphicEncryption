3cecae4c68e11a15452bc4e5e06f4950ab990f8f
n1ckl0s <25745329+n1ckl0sk0rtge@users.noreply.github.com>
2021-04-10 21:51:09 +0200

    delete package

--- SwiftHECryptoPackage/SwiftHECrypto/README.md
+++
@@ -1,3 +0,0 @@
-# SwiftHECrypto
-
-A description of this package.
--- SwiftHECryptoPackage/SwiftHECrypto/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import SwiftHECryptoTests
-
-var tests = [XCTestCaseEntry]()
-tests += SwiftHECryptoTests.allTests()
-XCTMain(tests)
